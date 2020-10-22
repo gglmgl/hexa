dbeaa62639f14e21eaa598767c9c992e89ac391e
DV <v.denisenko@gm>
2020-10-23 00:10:52 +0300

    add files

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(done) {
+    gulp.src("sass/*.scss")
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest("css"));
+    done();
+});
+
+gulp.task('serve', function(done) {
+    gulp.watch("sass/*.scss", gulp.series('sass'));
+    done();
+});
+
+gulp.task('default', gulp.series('sass', 'serve'));
